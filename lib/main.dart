b8afa7c610d0934b8bfff2753127b18ec44bdc1f
mohamed3sman <mohamedabdoosman12@gmail.com>
2023-04-19 17:04:38 +0200

    create splash view body

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:bookly_app/views/splash_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashView(),
+    );
+  }
+}
