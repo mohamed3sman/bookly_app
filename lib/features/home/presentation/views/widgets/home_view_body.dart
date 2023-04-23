f979d299127aea81a3ea678e0b82c69fd0f46364
mohamed3sman <mohamedabdoosman12@gmail.com>
2023-04-24 00:02:36 +0200

    best seller list view item-1

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,7 +1,7 @@
 import 'package:bookly_app/core/utils/styles.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/best_seller_list_view_item.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/books_list_view.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
-import 'package:bookly_app/features/home/presentation/views/widgets/custom_list_view_item.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -15,14 +15,12 @@ class HomeViewBody extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           const CustomAppBar(),
-          FeaturedBooksListView(),
+          const FeaturedBooksListView(),
           SizedBox(
             height: MediaQuery.of(context).size.height * 0.06,
           ),
-          const Text(
-            'Best Seller',
-            style: Styles.titleMedium,
-          )
+          const Text('Best Seller', style: Styles.titleMedium),
+          const BestSellerListViewItem(),
         ],
       ),
     );
