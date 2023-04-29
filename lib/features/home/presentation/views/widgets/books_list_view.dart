bcf9f5f4756a2c5dc6a4b51864f7eeda4be2ae38
mohamed3sman <mohamedabdoosman12@gmail.com>
2023-04-29 20:54:34 +0200

    display featured books part 1

--- lib/features/home/presentation/views/widgets/books_list_view.dart
+++ lib/features/home/presentation/views/widgets/books_list_view.dart
@@ -1,18 +1,34 @@
+import 'package:bookly_app/core/widgets/custom_error_message.dart';
+import 'package:bookly_app/core/widgets/custom_progress_indicator.dart';
+import 'package:bookly_app/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_list_view_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class FeaturedBooksListView extends StatelessWidget {
   const FeaturedBooksListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.3,
-      child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) {
-            return const CustomBookItem();
-          }),
+    return BlocBuilder<FeaturedBooksCubit, FeaturedBooksState>(
+      builder: (context, state) {
+        if (state is FeaturedBooksSuccess) {
+          return SizedBox(
+            height: MediaQuery.of(context).size.height * 0.3,
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return const CustomBookItem();
+                }),
+          );
+        } else if (state is FeaturedBooksFailure) {
+          return CustomErrorMessage(
+            errMessage: state.errorMessage,
+          );
+        } else {
+          return const CustomProgressIndicator();
+        }
+      },
     );
   }
 }
